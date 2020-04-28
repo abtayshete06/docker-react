a728f5ba8b030861db4209966d9420619dfcfdfa
abtayshete06 <45194742+abtayshete06@users.noreply.github.com>
2020-04-28 11:03:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hagrat</h1>
         </header>
         <p className="App-intro">
-          I was changed on the Feature branch
+          I Love you
         </p>
       </div>
     );
